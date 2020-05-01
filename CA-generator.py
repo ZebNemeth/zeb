f4edd203cb05dd44338d667671d8f54f2d9deae5
Zeb Németh <58953998+ZebNemeth@users.noreply.github.com>
2020-05-01 16:52:24 +0200

    Add files via upload
    Density added
    More efficient calculation

--- CA-generator.py
+++ CA-generator.py
@@ -1,80 +1,117 @@
 """
+   Zeb Nemeth
+   januari 2020
+
 
 Elementaire-cellulaire-automataplaatjesgenerator
-Geef hem je regelnummer en resolutie en klaar is kees!
+Geef hem je (regelnummer, dichtheid, breedte, hoogte) 
+en klaar is kees!
 
 
 """
 
 import random
 from PIL import Image
+import time
 
-
-# Physics creation, see "Wolfram Code for elementary CA"
-
-stateSpace = "L"     # string:   L for grayscale; not sleek yet.
-                     #           1 for "binary". Not sleek yet; 0 and 255...
-                     #           RGB for (0-255,",")
-
-state = (255, 0)
-rule = [ state[0],   # 111
-         state[1],   # 110
-         state[1],   # 101
-         state[0],   # 100
-         state[1],   # 011
-         state[0],   # 010
-         state[1],   # 001
-         state[0]    # 000
-         ]
-
-# Environment creation
-
-imageWidth = 1920
-imageHeight = 1080
-
-imgCA = Image.new( stateSpace, [imageWidth, imageHeight], 130) # 130; fill gray
-cell = imgCA.load()
-
-
-# Fill the initial state
-
-for x in range (0, imageWidth):
-   cell[x,0] = random.choice(state)
+beginCPUCycle = time.clock() # om efficientere code te testen
 
 
 
 
-# Evolve the universe
-
-for y in range (1, imageHeight):
-   for x in range (0, imageWidth):
 
 
-      # Trying not to let image index go out of range on the edges of the image
-      if ( x == 0 ):
-         parents = ( cell[imageWidth-1, y-1], cell[x, y-1], cell[x+1, y-1] )
-      elif ( x == imageWidth-1 ):
-         parents = ( cell[x-1, y-1], cell[x, y-1], cell[0, y-1] )
+
+def GenerateImage(ruleWolframCode, density, imageWidth, imageHeight):
+
+   # Physics creation, see "Wolfram Code for elementary CA"
+   """
+   hier kan nog veel aan gesleuteld worden voor multifunctionaliteit
+   """
+   stateSpace = "L"     # string:   L for grayscale; not sleek yet.
+                        #           1 for "binary". Not sleek yet; 0 and 255...
+                        #           RGB for (0-255,",")
+   stateAmount = 2
+
+   parentAmount = 3
+
+   state = (255, 0)
+
+
+   # Binarification, rule given
+
+   binaryRuleWolframCode = [0]*8
+   for i in range (7, -1, -1):
+      binaryPower = 2**i
+      a = ruleWolframCode - binaryPower
+      if ( a >= 0 ):
+         ruleWolframCode -= binaryPower
+         binaryRuleWolframCode[i] = 1
+         print (ruleWolframCode)
       else:
-         parents = ( cell[x-1, y-1], cell[x, y-1], cell[x+1, y-1] )
+         binaryRuleWolframCode[i] = 0
+   print ( binaryRuleWolframCode )
+
+   rule = [ state[binaryRuleWolframCode[0]],   # 000    ...namely binary parent description
+            state[binaryRuleWolframCode[1]],   # 001
+            state[binaryRuleWolframCode[2]],   # ..
+            state[binaryRuleWolframCode[3]],
+            state[binaryRuleWolframCode[4]],
+            state[binaryRuleWolframCode[5]],
+            state[binaryRuleWolframCode[6]],
+            state[binaryRuleWolframCode[7]]    # 111
+            ]
 
 
 
-      if   ( parents == (state[0],state[0],state[0]) ):
-         cell[x,y] = rule[7]
-      elif ( parents == (state[0],state[0],state[1]) ):
-         cell[x,y] = rule[6]
-      elif ( parents == (state[0],state[1],state[0]) ):
-         cell[x,y] = rule[5]
-      elif ( parents == (state[0],state[1],state[1]) ):
-         cell[x,y] = rule[4]
-      elif ( parents == (state[1],state[0],state[0]) ):
-         cell[x,y] = rule[3]
-      elif ( parents == (state[1],state[0],state[1]) ):
-         cell[x,y] = rule[2]
-      elif ( parents == (state[1],state[1],state[0]) ):
-         cell[x,y] = rule[1]
-      elif ( parents == (state[1],state[1],state[1]) ):
-         cell[x,y] = rule[0]
+   # Environment creation, width and height given
 
-imgCA.show()
+   imgCA = Image.new( stateSpace, [imageWidth, imageHeight], 130) # 130; fill gray
+   cell = imgCA.load()
+
+
+
+
+
+   # Fill the initial state. density given
+
+   for x in range (0, imageWidth):
+      cell[x,0] = random.randint(0,255)
+      if ( cell[x,0] < int(density*255) ):
+         cell[x,0] = state[1]
+      else:
+         cell[x,0] = state[0]
+
+
+
+
+
+
+   # Evolve the universe
+
+   for y in range (1, imageHeight):
+      for x in range (0, imageWidth):
+
+
+         # Trying not to let image index go out of range on the edges of the image
+         if ( x == 0 ):
+            cell[x,y] = rule[ 4 * ( cell[imageWidth-1, y-1] == state[1] ) +
+                              2 * ( cell[x, y-1] == state[1] ) +
+                              ( cell[x+1, y-1] == state[1] )
+                           ]
+         elif ( x == imageWidth-1 ):
+            cell[x,y] = rule[ 4 * ( cell[x-1, y-1] == state[1] ) +
+                              2 * ( cell[x, y-1] == state[1] ) +
+                              ( cell[0, y-1] == state[1] )
+                           ]
+         else:
+            cell[x,y] = rule[ 4 * ( cell[x-1, y-1] == state[1] ) +
+                              2 * ( cell[x, y-1] == state[1] ) +
+                              ( cell[x+1, y-1] == state[1] )
+                           ]
+
+   imgCA.show()
+
+GenerateImage(110,.5,960,540)
+
+print ( time.clock() - beginCPUCycle )
